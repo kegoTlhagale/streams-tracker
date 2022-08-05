22f7013dd0ea8c9fc6008c3b8d509604d14f2a75
kegoTlhagale <kegomoditswetlhagale@gmail.com>
2022-08-05 20:23:17 +0200

    wrote logic to track the streams

--- app/controllers/streams.controller.js
+++ app/controllers/streams.controller.js
@@ -1,77 +1,159 @@
 /* eslint-disable prefer-promise-reject-errors */
 import { HTTP_CODES } from '../globals.js'
-import Stream from '../models/stream.model.js'
-// import logger from '../helpers/logger.js'
+import Streams from '../models/stream.model.js'
+import logger from '../utilities/logger.js'
 
 const track = async (req, res) => {
-  const { user, streamsCounterUpdate } = req.body
+  const { streamid, streamsCounterUpdate } = req.body
   const permittedUpdates = [-1, 1]
 
-  if (!user) {
+  logger.log(`${streamid} Validating the payload`)
+  if (!streamid) {
     return res.status(HTTP_CODES.BAD_REQUEST).json({
       success: false,
-      message: 'Missing user id'
+      message: 'Missing stream id'
     })
   }
 
-  if (permittedUpdates.includes(streamsCounterUpdate)) {
+  // ensure that the update value is a 1 to increment the streams counter or a zero to decrement the streams counter
+  if (!permittedUpdates.includes(streamsCounterUpdate)) {
     return res.status(HTTP_CODES.BAD_REQUEST).json({
       success: false,
       message: 'Invalid or missing increment/decrement value'
     })
   }
 
-  /** Promise chain that does the following
-    - check if the level exists
-    - if the level exists, return a message that the level already exists
-    - if level does not exist, create it, and return a message that the level was created successfully
-  */
-
-  return new Promise((resolve, reject) => {
-    Stream.findOne({ user }, (error, existingUser) => {
-      if (error) {
-        return reject({
-          status: HTTP_CODES.SERVER_ERROR,
-          message: `Something went wrong while retrieving stream for account ${user}`
-        })
-      }
-
-      resolve(existingUser)
-    })
-  }).then((existingUser) => {
-    if (streamsCounterUpdate === 1 && existingUser.streamsCounter >= 3) {
-      /* throw error "too many concurrent streams" */
-    } else if (streamsCounterUpdate === 1 && existingUser.streamsCounter >= 3) {
-      /* throw error "too many concurrent streams" */
-    } else if (streamsCounterUpdate === -1 && existingUser.streamsCounter >= 3) {}
-
-    return res.status(HTTP_CODES.OK)
-      .json({
-        success: true,
-        data: { ...existingUser }
-      })
-  }).catch((error) => {
-    return res.status(error.status || HTTP_CODES.SERVER_ERROR)
+  logger.log(`${streamid} The payload is valid.`)
+  logger.log(`${streamid} Check if the stream already exists`)
+  Streams.findOne({ streamid }, (error, existingStream) => {
+    console.log('1')
+    if (error) {
+      logger.error(`${streamid}something went wrong while searching for the stream. \n`, error)
+      return res.status(HTTP_CODES.SERVER_ERROR)
       .json({
         success: false,
-        message: error.message || 'Oops... Something went wrong while trying to track stream.'
+        message:  `Oops... something went wrong. Please try again later`
       })
+    }
+
+    if (existingStream) {
+      logger.log(`${streamid} The stream exists`)
+      updateCurrentStreamsCounter(res, existingStream, streamsCounterUpdate)
+    } else {
+      logger.log(`${streamid} The stream does exists`)
+      createStream(res, streamid, streamsCounterUpdate)
+    }
   })
-
-  // if (userInDatabase && streamCountChange === 1 && currentStreams < 3) {
-  //     /*update userId's currentStreams + 1 in database*/
-  //   } else if (!userInDatabase && streamCountChange === 3) {
-  //     /*throw error "too many concurrent streams"*/
-  //   } else if (userInDatabase && streamCountChange === -1 && currentStreams === 1) {
-  //     /*delete row in database*/
-  //   } else if (userInDatabase && streamCountChange === -1 && currentStreams > 1) {
-  //     /*update userId's currentStreams - 1 in database*/
-  //   }
-
-//   return res.status(HTTP_CODES.OK)
-//     .json({
-//       success: true
-//     })
 }
+
+const createStream = (res, streamid, streamsCounterUpdate) => {
+  if (streamsCounterUpdate < 0) {
+    logger.log(`${streamid} There are no active streams tracked`)
+
+    return res.status(HTTP_CODES.OK)
+    .json({
+      success: true,
+      limitExceed: false,
+      streamid,
+      concurrentStreams: 0
+    })
+  }
+
+  if (streamsCounterUpdate === 1) {
+    logger.log(`${streamid} creating new stream`)
+
+    Streams.create({ streamid, streamsCounter: streamsCounterUpdate }, (error, newStream) =>{
+      if (error) {
+        logger.error(`${streamid} Something went wrong while creating new stream. \n`, error)
+  
+        return res.status(HTTP_CODES.SERVER_ERROR)
+        .json({
+          success: false,
+          message:  `Oops... something went wrong. Please try again later`
+        })
+      }
+  
+      let limitExceed = false
+      if (newStream.streamsCounter >= 3) {
+        limitExceed = true
+      }
+      return res.status(HTTP_CODES.OK)
+      .json({
+        success: true,
+        limitExceed,
+        streamid,
+        concurrentStreams: newStream.streamsCounter
+      })
+    })
+  }
+}
+
+const updateCurrentStreamsCounter = (res, existingStream, streamsCounterUpdate) => {
+
+  console.log('existingStream', existingStream)
+  console.log('streamsCounterUpdate', streamsCounterUpdate)
+
+  const { streamsCounter, streamid } = existingStream
+  logger.log(`${streamid} Checking if the number of concurrent streams has not exceeded 3`)
+
+  if (streamsCounterUpdate === 1 && streamsCounter >= 3) {
+    logger.log(`${streamid} Too many concurrent streams`)
+
+    // send response if there are already 3 stream. Also accounts for when there is more
+    return res.status(HTTP_CODES.OK)
+    .json({
+      success: true,
+      limitExceed: true,
+      streamid,
+      concurrentStreams: streamsCounter
+    })
+  } else {
+      if (streamsCounterUpdate === 1)
+        logger.log(`${streamid} Concurrent streams are still below the limit. Increment the number of streams by 1`)
+
+      if (streamsCounterUpdate === -1)
+        logger.log(`${streamid} A stream is no longer active. Decrement the number of streams by one`)
+        
+      // increment/decrement streams counter
+      let newStreamsCounter = streamsCounter + streamsCounterUpdate
+
+      // if the counter is a negative value, set it to zero
+      if (newStreamsCounter < 0) {
+        newStreamsCounter = 0
+      }
+
+      console.log('existingStream.streamsCounter', existingStream.streamsCounter)
+      console.log('streamsCounter', streamsCounter)
+      console.log('newStreamsCounter', newStreamsCounter)
+      
+       // update the counter in the db
+      existingStream.updateOne({ streamsCounter: newStreamsCounter },(error, updatedStream) => {
+        if (error) {
+          logger.error(`${streamid} Something went wrong while updating for the stream. \n`, error)
+    
+          return res.status(HTTP_CODES.SERVER_ERROR)
+          .json({
+            success: false,
+            message:  `Oops... something went wrong. Please try again later`,
+          })
+        }
+
+        console.log('updatedStream', updatedStream)
+        let limitExceed = false
+        if (updatedStream.streamsCounter >= 3) {
+          limitExceed = true
+        }
+        return res.status(HTTP_CODES.OK)
+        .json({
+          success: true,
+          limitExceed,
+          streamid,
+          concurrentStreams: newStreamsCounter
+        })
+      })
+  }
+}
+
+
 
 export { track }
